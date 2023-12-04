239d7711c7933e570516cd7bdfd42c38f57a10df
Vincent Testagrossa <lancevancevt@gmail.com>
2023-12-04 18:38:34 -0500

    Added scaffolding to create the functions for the page

---
+++ app.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book() {
+    //TODO: add constructor
+}
+
+function addBookToLibrary() {
+    //TODO: add book to library
+}
+
+function showLibrary(){
+    for (book in myLibrary){
+        //TODO: loop through each book in the library and display it on the page
+    }
+}
