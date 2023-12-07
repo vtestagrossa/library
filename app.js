d06f12a86a227246040fe004052cd1023fc40992
Vincent Testagrossa <lancevancevt@gmail.com>
2023-12-07 09:40:49 -0500

    general layout done. Need to fix grid sizing so that content-books doesn't change at all

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const myLibrary = [];
+const myLibrary = [new Book("The Hobbit", "J.R.R. Tolkien", "300", true)];
 
-function Book() {
+function Book(title, author, numPages, isRead) {
     //TODO: add constructor
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isRead = isRead;
 }
 
 function addBookToLibrary() {
