922297621e84f945a04205b75487625663463a8f
Vincent Testagrossa <lancevancevt@gmail.com>
2023-12-18 14:53:01 -0500

    changed all functions into classes

--- app.js
+++ app.js
@@ -1,87 +1,122 @@
-const myLibrary = [
-    new Book("The Hobbit", "J.R.R. Tolkien", 300, false)];
-    
-const addBtn = document.getElementById('addBtn');
-const closeBtn = document.getElementById('closeDiag');
-const submitBtn = document.getElementById('submitBtn');
-const dialog = document.getElementById('input-dialog');
-const content = document.getElementById('content-books');
-const form = document.getElementById('input-form');
-
-function Book(title, author, numPages, isRead) {
-    //TODO: add constructor
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.isRead = isRead;
-}
-function openDialog() {
-    const dialog = document.getElementById('input-dialog');
-    dialog.showModal();
-}
-function addBookToLibrary(){
-    if(!form.checkValidity()){
-        alert("Please fill out the required inputs!");
+class Book {
+    title = "";
+    author = "";
+    numPages = 0;
+    isRead = false;
+    constructor (title, author, numPages, isRead){
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.isRead = isRead;
     }
-    else{
-        myLibrary.push(new Book(
-            document.getElementById("title").value,
-            document.getElementById("author").value,
-            document.getElementById("pages").value,
-            document.getElementById("isRead").checked
+}
+
+Library = new class{
+    #library = [
+        new Book("The Hobbit", "J.R.R. Tolkien", 300, false)];
+    addBookToLibrary = (title, author, pages, isRead) => {
+        this.#library.push(new Book(
+            title,
+            author,
+            pages,
+            isRead
         ));
-        showLibrary(myLibrary);
+    }
+    removeBookFromLibrary = (idx) => {
+        this.library.splice(idx, 1);
+    }
+    get library() {
+        return this.#library;
+    }
+    /**
+     * @param {number} idx
+     */
+    set isRead(idx) {
+        this.#library[idx].isRead = !this.#library[idx].isRead;
+    }
+}
+Display = new class{
+    addBtn = document.getElementById('addBtn');
+    closeBtn = document.getElementById('closeDiag');
+    submitBtn = document.getElementById('submitBtn');
+    dialog = document.getElementById('input-dialog');
+    form = document.getElementById('input-form');
+    dialog = document.getElementById('input-dialog');
+    constructor(){
+        this.addBtn.addEventListener('click', () => {
+            this.showDialog();
+        });
+        this.closeBtn.addEventListener('click', () => {
+            this.hideDialog();
+        });
+        this.submitBtn.addEventListener('click', () => {
+            this.addBook();
+        });
+    }
+    showGrid = () => {
+        let content = document.getElementById('content-books');
+        this.clearGrid();
+        let idNum = 0;
+        Library.library.forEach(function(book){
+            let card = document.createElement('div');
+            card.classList.add('book');
+            content.appendChild(card);
+            card.appendChild(document.createElement('label'));
+            card.lastElementChild.textContent = "Title: " + book['title'];
+            card.appendChild(document.createElement('label'));
+            card.lastElementChild.textContent = "Author: " + book['author'];
+            card.appendChild(document.createElement('label'));
+            card.lastElementChild.textContent = "Pages: " + book['numPages'];
+            card.appendChild(document.createElement('div'));
+            card.lastElementChild.classList.add('book-group');
+            card.lastElementChild.appendChild(document.createElement('label'));
+            card.lastElementChild.lastElementChild.textContent = "Has Read? ";
+            card.lastElementChild.appendChild(document.createElement('input'));
+            card.lastElementChild.lastElementChild.setAttribute('type', 'checkbox');
+            //assign an id to the attribute of the checkbox so we can toggle the value later
+            card.lastElementChild.lastElementChild.setAttribute('id', idNum);
+            card.lastElementChild.lastElementChild.checked = book['isRead'];
+            card.lastElementChild.lastElementChild.addEventListener("click", (event) => {
+                Library.isRead = event.target.id;
+                Display.showGrid();
+            });
+            card.appendChild(document.createElement('button'));
+            card.lastElementChild.classList.add('addBtn');
+            card.lastElementChild.textContent = 'Remove';
+            card.lastElementChild.setAttribute('id', idNum);
+            card.lastElementChild.addEventListener('click', (event)=>{
+                Library.removeBookFromLibrary(event.target.id);
+                Display.showGrid();
+            });
+            idNum++;
+        });
+    }
+    clearGrid = () => {
+        let content = document.getElementById('content-books');
+        while(content.hasChildNodes()){
+            content.removeChild(content.firstChild);
+        }
+    }
+    showDialog = () => {
+        this.dialog.showModal();
+    }
+    hideDialog = () => {
+        this.dialog.close();
+    }
+    addBook = () => {
+        let title = document.getElementById("title").value;
+        let author = document.getElementById("author").value;
+        let pages = document.getElementById("pages").value;
+        let isRead = document.getElementById("isRead").checked;
+
+        if (!this.form.checkValidity()){
+            alert("Please fill out the required inputs!");
+        }
+        else {
+            Library.addBookToLibrary(title, author, pages, isRead);
+        }
+        this.showGrid();
     }
 }
 
-function showLibrary(){
-    clearGrid();
-    let idNum = 0;
-    myLibrary.forEach(function(book){
-        let card = document.createElement('div');
-        card.classList.add('book');
-        content.appendChild(card);
-        card.appendChild(document.createElement('label'));
-        card.lastElementChild.textContent = "Title: " + book['title'];
-        card.appendChild(document.createElement('label'));
-        card.lastElementChild.textContent = "Author: " + book['author'];
-        card.appendChild(document.createElement('label'));
-        card.lastElementChild.textContent = "Pages: " + book['numPages'];
-
-        card.appendChild(document.createElement('div'));
-        card.lastElementChild.classList.add('book-group');
-        card.lastElementChild.appendChild(document.createElement('label'));
-        card.lastElementChild.lastElementChild.textContent = "Has Read? ";
-        card.lastElementChild.appendChild(document.createElement('input'));
-        card.lastElementChild.lastElementChild.setAttribute('type', 'checkbox');
-        
-        //assign an id to the attribute of the checkbox so we can toggle the value later
-        card.lastElementChild.lastElementChild.setAttribute('id', idNum);
-        card.lastElementChild.lastElementChild.checked = book['isRead'];
-        card.lastElementChild.lastElementChild.addEventListener("click", (event) => {
-            myLibrary[event.target.id].isRead = event.target.checked;
-            showLibrary();
-        });
-
-        card.appendChild(document.createElement('button'));
-        card.lastElementChild.classList.add('addBtn');
-        card.lastElementChild.textContent = 'Remove';
-        card.lastElementChild.setAttribute('id', idNum);
-        card.lastElementChild.addEventListener('click', (event)=>{
-            myLibrary.splice(event.target.id, 1);
-            showLibrary();
-        });
-        idNum++;
-    });
-}
-function clearGrid(){
-    while(content.hasChildNodes()){
-        content.removeChild(content.firstChild);
-    }
-}
-addBtn.addEventListener("click", openDialog);
-submitBtn.addEventListener("click", addBookToLibrary);
-closeBtn.addEventListener("click", () => {
-    dialog.close();
-});
-showLibrary(myLibrary);
+Display.showGrid();
